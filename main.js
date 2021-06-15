0b8e6776feb702194e87c242a07aba27025d07a2
Karthik Balachandar <caarthik180@gmail.com>
2021-06-16 01:35:54 +0530

    Basic DOM Operations

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-const p = document.getElementById ('para1')
+const p = document.getElementById ('para1') 
 console.log (p)
+
+document.getElementsByTagName ('p')[0].innerHTML = "Fill to create an account"
+
+
+let q = document.getElementsByTagName ('p')[0]
+
+q.style.color = 'orange'
+
+q.style.fontSize = '35px'
+
+let r = document.getElementsByClassName('para2')[0]
+
+r.style.color ='red'
+r.style.fontSize = '20px'
+ 
