447feb0dc31a60e0a8a18118508ecc6710b576b8
Karthik Balachandar <caarthik180@gmail.com>
2021-06-16 18:25:10 +0530

    DOM Operations added

--- main.js
+++ main.js
@@ -14,4 +14,21 @@ let r = document.getElementsByClassName('para2')[0]
 
 r.style.color ='red'
 r.style.fontSize = '20px'
- 
+
+const s = document.getElementsByClassName('enter')
+
+for(i=0; i<s.length; i++){
+    s[i].style.color = 'blue'
+}
+
+let t = document.querySelector('.para2')[0]
+console.log(t)
+
+
+const body = document.getElementsByTagName('body')[0]
+const div = document.createElement('div')
+const u = document.createElement('p')
+
+u.innerHTML = "New Para"
+div.appendChild(u)
+body.appendChild(div)
