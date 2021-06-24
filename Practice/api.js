11447bbb9fe5c4cfcd4887a8947e3ac7b499b848
Karthik Balachandar <caarthik180@gmail.com>
2021-06-24 18:32:17 +0530

    api Example 2

--- Practice/api.js
+++ Practice/api.js
@@ -1,16 +1,44 @@
-fetch('https://jsonplaceholder.typicode.com/photos')
-.then((response) => response.json())
-.then (json => {
-    previewImage (json)
-})
+// fetch('https://jsonplaceholder.typicode.com/photos')
+// .then((response) => response.json())
+// .then (json => {
+//     previewImage (json)
+// })
 
- const previewImage = (json) => {
-     json.map((photo) => {
-         const li = document.createElement('li')
-         const img = document.createElement('img')
-         img.src = photo.url
-         img.alt= photo.title
-         li.appendChild(img)
-         document.getElementById('photosList').appendChild(li)
-     })
- } 
+//  const previewImage = (json) => {
+//      json.map((photo) => {
+//          const li = document.createElement('li')
+//          const img = document.createElement('img')
+//          img.src = photo.url
+//          img.alt= photo.title
+//          li.appendChild(img)
+//          document.getElementById('photosList').appendChild(li)
+//      })
+//  } 
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => response.json())
+.then((data) => {
+    data.map((details) => {
+        const userCard = document.createElement('div')
+        userCard.classList.add('card')
+        const userData = [
+            details.name,
+            details.email,
+            details.website,
+            details.phone,
+        ]
+
+        userData.map((detail) => {
+            const p = document.createElement('p')
+            p.innerHTML = detail
+            userCard.appendChild(p)
+        })
+
+        const li = document.createElement('li')
+        li.style.listStyleType = 'none'
+        li.appendChild(userCard)
+        document.getElementById('users').appendChild(li)
+    })
+})
